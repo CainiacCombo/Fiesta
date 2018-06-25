862b43534b1c918d7840a5b82d275283226158dd
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-25 16:36:27 -0500

    Added /users endpoint

---
+++ server/src/models/users.model.js
@@ -0,0 +1,28 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  users.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return users;
+};
