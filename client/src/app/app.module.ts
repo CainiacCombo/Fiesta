6799e793caa45501600fd0ca5f02776551275d3b
Taylor Shephard <lasha.tay@gmail.com>
2018-06-26 15:14:13 -0500

    impported tabs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,23 +5,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 import { SignupPageModule } from '../pages/signup/signup.module'
+import { TabsPageModule } from '../pages/tabs/tabs.module'
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     SignupPageModule,
+    TabsPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
   ],
   providers: [
     StatusBar,
