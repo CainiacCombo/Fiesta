9c1ccd5fc18099a7c24059ae86882fe8cf003686
Taylor Shephard <lasha.tay@gmail.com>
2018-06-26 12:14:19 -0500

    login function

--- client/src/pages/signup/signup.ts
+++ client/src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { HomePage } from '../home/home'
 
 @IonicPage()
 @Component({
@@ -10,6 +11,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login() {
+    // push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    console.log('logged in with google');
+    this.navCtrl.push(HomePage);
   }
 
   ionViewDidLoad() {
