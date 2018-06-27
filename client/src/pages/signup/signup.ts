0761cc4f8523cd914d9e7bc2bdc2747fd5aab2da
Taylor Shephard <taylorshephard1@gmail.com>
2018-06-27 16:52:16 -0500

    user info being saved from signup page

--- client/src/pages/signup/signup.ts
+++ client/src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home'
+import { HomePage } from '../home/home';
+import { UserProvider } from '../../providers/user/user';
 
 @IonicPage()
 @Component({
@@ -11,17 +12,26 @@ export class SignupPage {
 
   nickname: string = '';
   phone: string = '';
+  username: string = '';
   userData: any;
 
-  constructor(navParams: NavParams, public navCtrl: NavController) {
+  constructor(navParams: NavParams, public navCtrl: NavController, public userProvider: UserProvider) {
     this.userData = navParams.get('userData');
   }
 
   onSubmit() {
-    const { nickname, phone, username } = this;
+    const { nickname, phone, username, userData } = this;
+    const user = {
+      'username': username,
+      'nickname': nickname,
+      'phone': phone,
+      'googleId': userData.googleId,
+      'email': userData.email,
+      'avatar': userData.avatar,
+      'accessToken': userData.accessToken,
+    }
+    this.userProvider.createUser(user);
     this.navCtrl.push(HomePage);
-    // send use provider to send PUT to user
-    // console.log(this.userData);
   }
 
 }
