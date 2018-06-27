424803aec415d8610484a3976161f277f7bc2482
Cain <cainwatson@users.noreply.github.com>
2018-06-27 15:00:29 -0500

    (update) use users provider in signup

--- client/src/pages/signup/signup.ts
+++ client/src/pages/signup/signup.ts
@@ -11,14 +11,17 @@ export class SignupPage {
 
   nickname: string = '';
   phone: string = '';
+  userData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(navParams: NavParams, public navCtrl: NavController) {
+    this.userData = navParams.get('userData');
   }
 
   onSubmit() {
     const { nickname, phone } = this;
     this.navCtrl.push(HomePage);
     // send use provider to send PUT to user
+    console.log(this.userData);
   }
 
 }
