50f90a2a7031de2fb3b994ed0c32e3c51f57da6d
Cain <cainwatson@users.noreply.github.com>
2018-06-28 16:21:32 -0500

    (add) get user parties in home

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,20 +1,44 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IonicPage } from 'ionic-angular';
+
+import { Observable } from 'rxjs/Observable';
+import { pluck } from 'rxjs/operators/pluck';
+import { switchMap } from 'rxjs/operators/switchMap';
+
+import { User } from '../../interfaces/User';
+import { AppState } from '../../store/reducers';
+import { AddUserParties } from '../../store/parties/parties.actions';
+import { PartyProvider } from '../../providers/party/party';
 
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   timelineRoot = 'TimelinePage'
   searchRoot = 'SearchPage'
   profileRoot = 'ProfilePage'
-  rootParams: any = {}
 
-  constructor(navParams: NavParams) {
-    this.rootParams.user = navParams.get('user');
+  user$: Observable<User>
+
+  constructor(
+    public partyProvider: PartyProvider,
+    private store: Store<AppState>,
+  ) {
+    this.user$ = store.select('user');
+  }
+
+  ngOnInit() {
+    this.user$.pipe(
+      pluck('id'),
+      switchMap(id => this.partyProvider.getUserParties(id)),
+    )
+      .do(parties => this.store.dispatch(new AddUserParties(parties.data)))
+      .take(1)
+      .subscribe();
   }
 
 }
