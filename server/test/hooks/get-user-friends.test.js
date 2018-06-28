78e2c1c0762c7dda4898d18c8c415a9b3c8941d1
Cain <cainwatson@users.noreply.github.com>
2018-06-28 09:40:37 -0500

    (test) get user friends hook

--- server/test/hooks/get-user-friends.test.js
+++ server/test/hooks/get-user-friends.test.js
@@ -4,24 +4,44 @@ const getUserFriends = require('../../src/hooks/get-user-friends');
 
 describe('\'get-user-friends\' hook', () => {
   let app;
+  let users;
+  let userFriends;
+  let user1Friends;
+  let user2Friends;
 
   beforeEach(() => {
     app = feathers();
+    users = {
+      1: { id: 1, username: 'bob' },
+      2: { id: 2, username: 'taylor' },
+    };
+    userFriends = {
+      total: 1,
+      limit: 10,
+      skip: 0,
+      data: [{ user1_id: 1, user2_id: 2 }],
+    };
+    user1Friends = { ...userFriends, data: [users[2]] };
+    user2Friends = { ...userFriends, data: [users[1]] };
 
-    app.use('/dummy', {
-      async get(id) {
-        return { id };
-      }
-    });
+    app.use('/users', { get: async id => users[id] });
+    app.use('/friends', { find: async () => userFriends });
+    app.use('/dummy', { find: () => Promise.reject(new Error('"user_id" not in users fieldlist')) });
 
     app.service('dummy').hooks({
-      after: getUserFriends()
+      error: getUserFriends(),
     });
   });
 
-  it('runs the hook', async () => {
-    const result = await app.service('dummy').get('test');
+  it('returns the friends for a user1', async () => {
+    const user1 = await app.service('dummy').find({ query: { user_id: 1 } });
 
-    assert.deepEqual(result, { id: 'test' });
+    assert.deepEqual(user1, user1Friends);
+  });
+
+  it('returns the friends for a user2', async () => {
+    const user2 = await app.service('dummy').find({ query: { user_id: 2 } });
+
+    assert.deepEqual(user2, user2Friends);
   });
 });
