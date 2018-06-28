d7727fa26d8c68ac109d482b283d375eba65817f
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-28 09:06:34 -0500

    generated media endpoint

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -4,6 +4,7 @@ const parties = require('./parties/parties.service.js');
 const messages = require('./messages/messages.service.js');
 const groupMessages = require('./group-messages/group-messages.service.js');
 const groupUsers = require('./group-users/group-users.service.js');
+const media = require('./media/media.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
@@ -12,4 +13,5 @@ module.exports = function (app) {
   app.configure(messages);
   app.configure(groupMessages);
   app.configure(groupUsers);
+  app.configure(media);
 };
