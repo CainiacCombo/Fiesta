b15ac95c7ba44158f2530dc12913a54f992ac310
Cain <cainwatson@users.noreply.github.com>
2018-06-28 09:40:26 -0500

    (test) remove friend request

--- server/test/hooks/remove-friend-request.test.js
+++ server/test/hooks/remove-friend-request.test.js
@@ -4,24 +4,41 @@ const removeFriendRequest = require('../../src/hooks/remove-friend-request');
 
 describe('\'remove-friend-request\' hook', () => {
   let app;
+  let user1_id;
+  let user2_id;
 
   beforeEach(() => {
     app = feathers();
+    user1_id = 1;
+    user2_id = 2;
 
-    app.use('/dummy', {
-      async get(id) {
-        return { id };
-      }
-    });
+    app.use('/dummy', { create: async () => {} });
 
     app.service('dummy').hooks({
-      before: removeFriendRequest()
+      before: removeFriendRequest(),
     });
   });
 
-  it('runs the hook', async () => {
-    const result = await app.service('dummy').get('test');
-    
-    assert.deepEqual(result, { id: 'test' });
+  it('returns error when there was no friend request', async () => {
+    app.use('/friend-requests', { find: async () => ({ total: 0 }), });
+
+    assert.rejects(
+      app.service('dummy').create({ user1_id, user2_id }),
+      `Could not find a friend request between "${user1_id}" and "${user2_id}"`
+    );
+  });
+
+  it('deletes a friend request', async () => {
+    const friendRequestId = 12;
+    let deletedFriendRequest;
+
+    app.use('/friend-requests', {
+      find: async () => ({ total: 1, data: [{ id: friendRequestId }] }),
+      remove: async id => deletedFriendRequest = id,
+    });
+
+    await app.service('dummy').create({ user1_id, user2_id });
+
+    assert.deepEqual(deletedFriendRequest, friendRequestId);
   });
 });
