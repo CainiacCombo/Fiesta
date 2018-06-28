e59f490e34e704fbd030545d977cfff89fee4767
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-28 10:18:38 -0500

    no unused vars

--- server/src/models/dm.model.js
+++ server/src/models/dm.model.js
@@ -1,6 +1,5 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-const Sequelize = require('sequelize');
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
