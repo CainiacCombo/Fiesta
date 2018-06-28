7127c1c1575caae736266292a9351b6a6ceee99f
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-28 10:16:22 -0500

    fixed linting error

--- server/src/models/dm.model.js
+++ server/src/models/dm.model.js
@@ -1,7 +1,6 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 const Sequelize = require('sequelize');
-const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
