d2351ec73f7c817b511d0d3870b9f5ecd949b6d8
Cain <cainwatson@users.noreply.github.com>
2018-06-28 12:11:42 -0500

    (update) dispatch login on signup and login pages

--- client/src/pages/login/login.ts
+++ client/src/pages/login/login.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 import { IonicPage, NavController, LoadingController } from 'ionic-angular';
 
+import { AppState } from '../../store/reducers';
+import { User } from '../../interfaces/User';
+import { UserProvider } from '../../providers/user/user';
 import { HomePage } from '../home/home';
 import { SignupPage } from '../signup/signup';
-import { UserProvider } from '../../providers/user/user';
+import { Login } from '../../store/user/user.actions';
 
 @IonicPage()
 @Component({
@@ -12,14 +17,22 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public userProvider: UserProvider) { }
+  user$: Observable<User>
+
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public userProvider: UserProvider,
+    private store: Store<AppState>,
+  ) { }
 
   authenticate(userData, findBy, authentication) {
     this.userProvider.findUser(findBy)
       .then(user => !user
         ? this.navCtrl.push(SignupPage, { userData })
-        : this.userProvider.authenticate(authentication)
-          .then(() => this.navCtrl.setRoot(HomePage, { user }, { animate: true, direction: 'right' })))
+        : this.userProvider.authenticate(user.id, authentication)
+           .then(user => this.store.dispatch(new Login(user)))
+           .then(() => this.navCtrl.setRoot(HomePage, { user }, { animate: true, direction: 'right' })))
   }
 
   googleLogin() {
@@ -30,7 +43,7 @@ export class LoginPage {
         strategy: 'google',
         access_token: user.accessToken,
       }))
-      .then(()=>loader.dismiss());
+      .then(() => loader.dismiss());
   }
 
   twitterLogin() {
