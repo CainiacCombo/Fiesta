97ef25f4edacfe55221b25efdabff3e504d0c534
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-28 09:02:32 -0500

    created group mesages service

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const friends = require('./friends/friends.service.js');
 const parties = require('./parties/parties.service.js');
 const messages = require('./messages/messages.service.js');
+const groupMessages = require('./group-messages/group-messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(friends);
   app.configure(parties);
   app.configure(messages);
+  app.configure(groupMessages);
 };
