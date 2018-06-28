a7c3ed174703c731e6654f335caf2299c60494c8
Cain <cainwatson@users.noreply.github.com>
2018-06-28 15:49:19 -0500

    (create) parties reducer

---
+++ client/src/providers/party/party.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { app } from '../../feathers';
+
+@Injectable()
+export class PartyProvider {
+
+  constructor(public http: HttpClient) { }
+
+  getUserParties(uid): Promise<any> {
+    return app.service('group-users').find({ query: { user_id: uid } });
+  }
+
+}
--- client/src/store/reducers.ts
+++ client/src/store/reducers.ts
@@ -1,10 +1,15 @@
 import { User } from '../interfaces/User';
+import { Party } from '../interfaces/Party';
+
 import { userReducer } from './user/user';
+import { partiesReducer } from './parties/parties';
 
 export interface AppState {
   user: User,
+  parties: Party[],
 }
 
 export const reducers = {
   user: userReducer,
+  parties: partiesReducer,
 }
