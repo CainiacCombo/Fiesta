c133fdc4aaa2ed121366aa1ed2cf7f9c41e06907
Cain <cainwatson@users.noreply.github.com>
2018-06-27 22:27:28 -0500

    (create) friend requests service

---
+++ server/src/services/friend-requests/friend-requests.hooks.js
@@ -0,0 +1,35 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const getFriendRequestUsers = require('../../hooks/get-friend-request-users');
+
+module.exports = {
+  before: {
+    all: [ authenticate('jwt') ],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [ getFriendRequestUsers() ],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
