3ec3433bcda930b2584c2cb518243f5b0f042304
Cain <cainwatson@users.noreply.github.com>
2018-06-28 22:16:22 -0500

    (cleanup) remove unused import

--- client/src/pages/home/sidebar/sidebar.ts
+++ client/src/pages/home/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { Store } from '@ngrx/store';
 
