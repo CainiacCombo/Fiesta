e03f2669c3737193087c828a48fbcea9d6d52ba1
Cain <cainwatson@users.noreply.github.com>
2018-06-29 09:25:41 -0500

    (update) populate to include fake gids

--- server/scripts/populate.sql
+++ server/scripts/populate.sql
@@ -3,11 +3,11 @@ USE fiesta;
 -- USERS
 DELETE FROM users;
 
-INSERT INTO users (id, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (1, "bob", "Bobby Boy", "https://images-na.ssl-images-amazon.com/images/M/MV5BODYzNzg4MjAxMF5BMl5BanBnXkFtZTgwNDEyODQ0MzI@._CR792,139,275,275_UX402_UY402._SY201_SX201_AL_.jpg", "I am bob, bob i am", "bob@hotmail.to", "444-1111", 3, NOW(), NOW());
-INSERT INTO users (id, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (2, "eric", "Super Swifter", "https://m.media-amazon.com/images/M/MV5BMTM3NjU2NTY0NF5BMl5BanBnXkFtZTcwNDI0NjgyMQ@@._V1_UY317_CR26,0,214,317_AL_.jpg", "I love Taylor Swift <3", "eric@tayor.swift", "444-2222", 3, NOW(), NOW());
-INSERT INTO users (id, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (3, "Taylor Swift", "T-Swizzle", "http://theweeklyspoon.com/wp-content/uploads/2018/05/F814A5D3-D18F-4A2F-81D2-D385431B1C92.jpeg", "Hey its your local country girl Taylor!!", "taylorswift@taylorswift.taylorswift", "444-3333", 5, NOW(), NOW());
-INSERT INTO users (id, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (4, "bob-backup-1", "Bob's Backup Acc 1", "<some avatar url>", "Backup #1 for @bob", "bob-1@bob.mail", "444-1112", 1, NOW(), NOW());
-INSERT INTO users (id, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (5, "bob-backup-2", "Bob's Backup Acc 2", "<some avatar url>", "Backup #2 for @bob", "bob-2@bob.mail", "444-1113", 1, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (1, "fake-google-id-1", "bob", "Bobby Boy", "https://images-na.ssl-images-amazon.com/images/M/MV5BODYzNzg4MjAxMF5BMl5BanBnXkFtZTgwNDEyODQ0MzI@._CR792,139,275,275_UX402_UY402._SY201_SX201_AL_.jpg", "I am bob, bob i am", "bob@hotmail.to", "444-1111", 3, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (2, "fake-google-id-2", "eric", "Super Swifter", "https://m.media-amazon.com/images/M/MV5BMTM3NjU2NTY0NF5BMl5BanBnXkFtZTcwNDI0NjgyMQ@@._V1_UY317_CR26,0,214,317_AL_.jpg", "I love Taylor Swift <3", "eric@tayor.swift", "444-2222", 3, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (3, "fake-google-id-3", "Taylor Swift", "T-Swizzle", "http://theweeklyspoon.com/wp-content/uploads/2018/05/F814A5D3-D18F-4A2F-81D2-D385431B1C92.jpeg", "Hey its your local country girl Taylor!!", "taylorswift@taylorswift.taylorswift", "444-3333", 5, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (4, "fake-google-id-4", "bob-backup-1", "Bob's Backup Acc 1", "<some avatar url>", "Backup #1 for @bob", "bob-1@bob.mail", "444-1112", 1, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (5, "fake-google-id-5", "bob-backup-2", "Bob's Backup Acc 2", "<some avatar url>", "Backup #2 for @bob", "bob-2@bob.mail", "444-1113", 1, NOW(), NOW());
 
 -- FRIENDS
 DELETE FROM friends;
