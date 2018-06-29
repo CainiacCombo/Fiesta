33e0c9f36d0edfe073a9431c0a1c538b284e1d41
Cain <cainwatson@users.noreply.github.com>
2018-06-29 14:50:19 -0500

    (fix) twilio config

--- server/src/twilio.js
+++ server/src/twilio.js
@@ -1,8 +1,9 @@
 const Twilio = require('twilio');
 const config = require('../config/default.json');
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID || config.twilio;
-const authToken = process.env.TWILIO_AUTH_TOKEN || config.twilio;
+const accountSid = process.env.TWILIO_ACCOUNT_SID || config.twilio.accountSid;
+const authToken = process.env.TWILIO_AUTH_TOKEN || config.twilio.authToken;
+
 const client = new Twilio(accountSid, authToken);
 
 module.exports = client;
