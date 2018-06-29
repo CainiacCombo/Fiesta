2bb1eb84f26db995efa139bb41613acfbfb9a4ab
Cain <cainwatson@users.noreply.github.com>
2018-06-28 22:15:23 -0500

    (update) type default reducer states

--- client/src/store/user/user.ts
+++ client/src/store/user/user.ts
@@ -1,11 +1,12 @@
+import { User } from '../../interfaces/User';
 import { UserActions, UserTypes } from './user.actions';
 
-const defaultState = {};
+const defaultState: User = null;
 
 export const userReducer = (state = defaultState, action: UserActions) => {
   switch (action.type) {
     case UserTypes.LOGIN:
-      return action.payload
+      return action.payload;
 
     case UserTypes.UPDATE:
       return {
