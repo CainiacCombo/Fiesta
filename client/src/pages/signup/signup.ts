a0352f7c2c2299b9d4983703efa65411d295a0a2
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-29 12:35:02 -0500

    fixed errors

--- client/src/pages/signup/signup.ts
+++ client/src/pages/signup/signup.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
-import { UserProvider } from '../../providers/user/user';
-import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
