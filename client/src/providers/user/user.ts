a9a6b3ce0d0b156e108a70fb70aee467d580f31b
Cain <cainwatson@users.noreply.github.com>
2018-07-03 18:36:58 -0500

    (update) use gplus native for auth

--- client/src/providers/user/user.ts
+++ client/src/providers/user/user.ts
@@ -1,57 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { app } from '../../feathers';
 import { User } from '../../interfaces/User';
 import { GoogleUser } from '../../interfaces/GoogleUser';
-import { app } from '../../feathers';
 
-declare const gapi: any;
-const GAPI_KEY = 'AIzaSyA5ywuy4AD7qzXvEqskL6Rzt3QFPFD0Aws';
-const GAPI_CLIENT_ID = '508966744275-71kg0n4705b07hg8eembqfi4jc84q10q.apps.googleusercontent.com';
-
-const initializeGoogle = () => new Promise((resolve, reject) => {
-  gapi.load('client:auth2', () => {
-    gapi.client.init({
-      apiKey: GAPI_KEY,
-      clientId: GAPI_CLIENT_ID,
-      scope: 'profile'
-    }).then(resolve).catch(reject);
-  });
+const createGoogleProfile = (payload): GoogleUser => ({
+  accessToken: payload.accessToken,
+  googleId: payload.userId,
+  email: payload.email,
+  name: payload.displayName,
+  avatar: payload.imageUrl,
 });
-
-const createGoogleProfile = (payload): GoogleUser => {
-  const auth = payload.getAuthResponse();
-  const accessToken = auth.access_token;
-
-  const user = payload.getBasicProfile();
-  const googleId = user.getId();
-  const name = user.getName();
-  const email = user.getEmail();
-  const avatar = user.getImageUrl();
-
-  return {
-    accessToken,
-    googleId,
-    email,
-    name,
-    avatar,
-  };
-};
 
 @Injectable()
 export class UserProvider {
 
-  constructor(public http: HttpClient) { }
+  constructor(public googlePlus: GooglePlus) { }
 
   authenticate(id, access_token): Promise<User> {
     return app.authenticate({ strategy: 'google', access_token })
       .then(() => this.findUser({ id }));
   }
 
-  findUser(findBy) {
+  findUser(findBy): Promise<User> {
     return app.service('users').find({ query: findBy }).then(response => response.data[0]);
   }
 
-  createUser(data) {
+  createUser(data): Promise<User> {
     return app.service('users').create(data);
   }
 
@@ -59,19 +34,12 @@ export class UserProvider {
     return app.service('users').patch(id, data);
   }
 
-  googleSignin() {
-    return initializeGoogle()
-      .then(() => {
-        const instance = gapi.auth2.getAuthInstance();
-        return instance.isSignedIn.get()
-          ? instance.currentUser.get()
-          : instance.signIn();
-      })
-      .then(createGoogleProfile);
+  googleSignin(): Promise<GoogleUser> {
+    return this.googlePlus.login({}).then(createGoogleProfile);
   }
 
-  googleSignout(): Promise<any> {
-    return gapi.auth2.getAuthInstance().signOut();
+  googleSignout() {
+    return this.googlePlus.disconnect();
   }
 
 }
