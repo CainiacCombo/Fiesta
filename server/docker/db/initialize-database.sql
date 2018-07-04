f4a5c8f3ee4b45da65567a29212689abc20d474f
Cain <cainwatson@users.noreply.github.com>
2018-07-03 19:57:07 -0500

    (fix) add reset db to init db sql

--- server/docker/db/initialize-database.sql
+++ server/docker/db/initialize-database.sql
@@ -4,3 +4,6 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
 flush privileges;
+
+DROP DATABASE fiesta
+CREATE DATABASE fiesta
