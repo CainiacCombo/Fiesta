3e6f10b311b9b57690aece90dba0cf7ded41dc1b
Eric O'Neal Jr <ejeric23@gmail.com>
2018-07-05 15:21:05 -0500

    implemented incept state

--- client/src/stateChanger.js
+++ client/src/stateChanger.js
@@ -3,5 +3,15 @@
   const game = window.match.game;
   game.init = function () {
     let _state;
-  }
+
+    const states = {
+      incept: Object.assign(aState('incept'), {
+        play() {
+          // get user's high scores, then go to lobby //
+
+          setState(states.lobby);
+        }
+      }),
+  },
+}
 })(window);
