036f94351ddb4281a81dbaf4f667d689716d3cce
Operation Spark <operationspark@Operations-MacBook-Pro-5.local>
2018-07-06 13:55:22 -0500

    profile will show actual count of hosted and attended parties

--- client/src/components/profile/profile.ts
+++ client/src/components/profile/profile.ts
@@ -1,26 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewController } from 'ionic-angular';
 import { User } from '../../interfaces/User';
 import { Party } from '../../interfaces/Party';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'profile',
   templateUrl: 'profile.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
   text: string;
   user: User;
-  pary: Party;
+  parties: Party[] = []
+  partiesHosted: Party[] = []
 
   constructor(
     public view: ViewController, 
     public navParams: NavParams,
+    public userProvider: UserProvider,
   ){
     this.user = navParams.get('user');
   }
 
+  ngOnInit() {
+    this.userProvider.getUserParties(this.user.id)
+      .then((parties) => {
+        this.parties = parties.data.map(groupUser => groupUser.party);
+        this.partiesHosted = parties.data
+          .filter(groupUser => groupUser.is_host === true)
+          .map(groupUser => groupUser.party);
+      });
+      
+  }
+  
   closeModal() {
     this.view.dismiss();
   }
