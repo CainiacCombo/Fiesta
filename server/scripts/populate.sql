26318cb2a487e196029cd186c1d1d8f5b10b5379
Cain <cainwatson@users.noreply.github.com>
2018-07-11 00:53:55 -0500

    (update) populate

--- server/scripts/populate.sql
+++ server/scripts/populate.sql
@@ -5,10 +5,10 @@ DELETE FROM users;
 
 INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (1, "fake-google-id-1", "bob", "Bobby Boy", "https://images-na.ssl-images-amazon.com/images/M/MV5BODYzNzg4MjAxMF5BMl5BanBnXkFtZTgwNDEyODQ0MzI@._CR792,139,275,275_UX402_UY402._SY201_SX201_AL_.jpg", "I am bob, bob i am", "bob@hotmail.to", "444-1111", 3, NOW(), NOW());
 INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (2, "fake-google-id-2", "eric", "Super Swifter", "https://m.media-amazon.com/images/M/MV5BMTM3NjU2NTY0NF5BMl5BanBnXkFtZTcwNDI0NjgyMQ@@._V1_UY317_CR26,0,214,317_AL_.jpg", "I love Taylor Swift <3", "eric@tayor.swift", "444-2222", 3, NOW(), NOW());
-INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (3, "fake-google-id-3", "Taylor Swift", "T-Swizzle", "http://theweeklyspoon.com/wp-content/uploads/2018/05/F814A5D3-D18F-4A2F-81D2-D385431B1C92.jpeg", "Hey its your local country girl Taylor!!", "taylorswift@taylorswift.taylorswift", "444-3333", 5, NOW(), NOW());
-INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (4, "fake-google-id-4", "bob-backup-1", "Bob's Backup Acc 1", "<some avatar url>", "Backup #1 for @bob", "bob-1@bob.mail", "444-1112", 1, NOW(), NOW());
-INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (5, "fake-google-id-5", "bob-backup-2", "Bob's Backup Acc 2", "<some avatar url>", "Backup #2 for @bob", "bob-2@bob.mail", "444-1113", 1, NOW(), NOW());
-INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (6, "109322987592954020059", "tay", "tay", "https://lh6.googleusercontent.com/-FdokHVutAbw/AAAAAAAAAAI/AAAAAAAAAAA/AB6qoq1ogk_liUaFlfHJAvHfY770sYXsLw/s96-c/photo.jpg", "yup", "taylorshephard1@gmail.com", "444-4444", 5, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (3, "fake-google-id-3", "therealtaylorswift", "T-Swizzle", "http://theweeklyspoon.com/wp-content/uploads/2018/05/F814A5D3-D18F-4A2F-81D2-D385431B1C92.jpeg", "Hey its your local country girl Taylor!!", "taylorswift@taylorswift.taylorswift", "444-3333", 5, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (4, "fake-google-id-4", "sasha", "Sasha", "https://vignette.wikia.nocookie.net/bratzmagicschool/images/3/36/Sasha.jpg/revision/latest?cb=20111230024851", "It's Sasha", "sasha@gooooooogle.com", "444-1112", 1, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (5, "fake-google-id-5", "andy", "Andy Nguyen", "https://ca.slack-edge.com/T02P3HQD6-U867E4UUA-800f2ed4f4f6-512", "Holy Molly! This app is great!", "andynguyen@andynguen.andynguen", "444-1113", 1, NOW(), NOW());
+INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (6, "107952859027055542447", "taylor", "tay", "https://lh3.googleusercontent.com/-9AiqmkZ7XRw/AAAAAAAAAAI/AAAAAAAAGmo/w_c6YFX6q2o/s120/photo.jpg", "Patience is VERY important...i'm talking to @cain", "lasha.tay@gmail.com", "444-4444", 5, NOW(), NOW());
 INSERT INTO users (id, googleId, username, nickname, avatar, bio, email, phone, rating, createdAt, updatedAt) VALUES (7, "111953930250628635331", "cainwatson", "Cain", "https://lh3.googleusercontent.com/-4nuxFupHIww/AAAAAAAAAAI/AAAAAAAAADY/GO0evWqwDeQ/s96-c/photo.jpg", "Hey, it's me Cain", "cainwatson@gmail.com", "444-5555", 5, NOW(), NOW());
 
 -- FRIENDS
@@ -23,6 +23,12 @@ DELETE FROM friend_requests;
 
 INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (4, 2, NOW(), NOW());
 INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (5, 2, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (1, 7, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (2, 7, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (3, 7, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (4, 7, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (5, 7, NOW(), NOW());
+INSERT INTO friend_requests (from_user_id, to_user_id, createdAt, updatedAt) VALUES (6, 7, NOW(), NOW());
 
 -- PARTIES
 DELETE FROM parties;
