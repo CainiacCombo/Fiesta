c466d03cef9bedbdb3ba7ad5ac1172ef9cd78760
Cain <cainwatson@users.noreply.github.com>
2018-07-11 03:52:47 -0500

    (add) conditionally render action button on profile page

--- client/src/components/profile/profile.ts
+++ client/src/components/profile/profile.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { App, NavParams, NavController, ViewController, ModalController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
 import { User } from '../../interfaces/User';
 import { Party } from '../../interfaces/Party';
-import { UserProvider } from '../../providers/user/user';
 import { AppState } from '../../store/reducers';
 import { Logout } from '../../store/user/user.actions';
+import { UserProvider } from '../../providers/user/user';
 import { LoginPage } from '../../pages/login/login';
 
 @Component({
@@ -15,8 +17,12 @@ import { LoginPage } from '../../pages/login/login';
 export class ProfileComponent implements OnInit {
 
   @Input('user') inUser: User
-  @Input('isUserProfile') isUserProfile: boolean
+  @Input('isProfilePage') isProfilePage: boolean
+
   user: User
+  isUserProfile: boolean
+  isFriendOfCurrentUser$: Observable<boolean>
+  hasAskedToFriend$: Observable<boolean>
   friends: User[] = []
   parties: Party[] = []
   partiesHosted: Party[] = []
@@ -33,12 +39,27 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.inUser || this.navParams.get('user');
+  }
+
+  ionViewWillLoad() {
+    if (!this.user) {
+      this.user = this.inUser || this.navParams.get('user');
+    }
+
+    this.store.select('user')
+      .take(1)
+      .subscribe(user => this.isUserProfile = this.isUserProfile || user.id === this.user.id);
+
+    this.isFriendOfCurrentUser$ = this.store.select('friends').pipe(
+      map(friends => !!friends.find(friend => friend.id === this.user.id))
+    );
+
+    this.hasAskedToFriend$ = this.store.select('friend-requests').pipe(
+      map(friendsRequests => !!friendsRequests.find(request => request.from_user_id === this.user.id.toString()))
+    );
 
     this.userProvider.getUserFriends(this.user.id)
-      .then(friends => {
-        console.log(this.user.id, JSON.stringify(friends.data));
-        this.friends = friends.data
-      });
+      .then(friends => this.friends = friends.data);
 
     this.userProvider.getUserParties(this.user.id)
       .then((parties) => {
@@ -64,7 +85,25 @@ export class ProfileComponent implements OnInit {
   signout() {
     this.userProvider.googleSignout()
       .then(() => this.app.getRootNav().setRoot(LoginPage, null, { animate: true, direction: 'left' }))
-      .then(() => this.store.dispatch(new Logout()))
+      .then(() => this.store.dispatch(new Logout()));
+  }
+
+  acceptFriendRequest() {
+    console.log('acceptFriendRequest!');
+    // await this.userProvider.acceptFriendRequest(from_user_id, to_user_id);
+    // dispatch new friends/friendRequests
+  }
+
+  sendFriendRequest() {
+    console.log('sendFriendRequest!');
+    // await this.userProvider.sendFriendReqest(store user.id, this.user.id);
+    // dispatch new friendsRequests
+  }
+
+  unfriend() {
+    console.log('unfriend!');
+    // await this.userProvider.unfriend(store user.id, this.user.id);
+    // dispatch new friends
   }
 
 }
