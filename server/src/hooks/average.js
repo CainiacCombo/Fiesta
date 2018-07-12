6c73dfcdf605c73715d1cc450aaa8055b7ee81e3
Eric O'Neal Jr <ejeric23@gmail.com>
2018-07-12 12:43:20 -0500

    deleted console.log

--- server/src/hooks/average.js
+++ server/src/hooks/average.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => async context => {
   const party_id = context.result.party_id;
-  console.log(party_id);
   const response = await context.app.service('party-ratings').find({ query: { party_id } });
 
   const allRating = response.data.reduce((rating, party) => party.rating + rating, 0);
@@ -20,7 +19,6 @@ module.exports = (options = {}) => async context => {
   });
 
   context.result.newHostRatings = await Promise.all(allHost.data.map(async (user) => { 
-    console.log(user);
     const userParties = await context.app.service('group-users').find({
       query: {
         user_id: user.user_id,
@@ -33,7 +31,6 @@ module.exports = (options = {}) => async context => {
     }, 0);
 
     const avgRating = totalUserPartyRating / userParties.total;
-    console.log('avg', avgRating);
     const patchedRating = await context.app.service('users').patch(user.user_id, {
       rating: avgRating,
     });
